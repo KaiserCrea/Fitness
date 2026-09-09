7e2a3cbd953cbca98ab18e5ea570157b787686f1
KaiserCrea <testarossa1383@gmail.com>
2026-09-09 15:51:00 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'fitness-v01';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
