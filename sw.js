36a449fdcfce3f424d3344c0671569aaf4d8b6aa
KaiserCrea <testarossa1383@gmail.com>
2026-09-21 22:23:30 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,9 @@
-const CACHE = "fitness-v24-25-fixes-20260921-1";
+const CACHE = "fitness-v24-25-1-card-safety-20260921-1";
 const IMAGE_CACHE = "fitness-static-images-v24185";
 const ROOT = [
   "./sheet-layouts.js",
   "./thumbnail-map.js",
-  "./assets/card-g1-approved-v2425.png",
-  "./assets/card-g2-approved-v2425.png",
+  "./miniatures/G1A/05_curl_biceps_barre_EZ_debout_v24251.png",
   "./assets/card-g1-v22.jpg",
   "./assets/card-g1-v22.png",
   "./assets/card-g2-v22.jpg",
